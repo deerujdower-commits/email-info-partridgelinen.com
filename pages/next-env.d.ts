7da91daf0ce7a5690bb5fccf752455729c34674a
deerujdower-commits <deerujdower@gmail.com>
2026-01-22 20:57:14 +0000

    chore: add next-env.d.ts
    Add type references for Next.js environment.

---
+++ pages/next-env.d.ts
@@ -0,0 +1,1 @@
+/// <reference types="next" /> /// <reference types="next/types/global" /> /// <reference types="next/image-types/global" /> // NOTE: This file should be in the project root and is required by Next.
